3c1a6c738f4d231e681afbd6b8dd8b9b7ee35375
gph03n1x <giannispapcod7@gmail.com>
2018-07-22 12:05:53 +0300

    Implemented cart dummy functionality

--- books/urls.py
+++ books/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path('book/<pk>', views.BookDetailView.as_view(), name='book_detail'),
-    path('my/', views.MyBooksListView.as_view(), name='profile'),
-    path('cart/', views.CartListView.as_view(), name='profile'),
+    path('book/<pk>/cart/', views.add_book_to_cart, name='book_to_cart'),
+    path('my/', views.MyBooksListView.as_view(), name='my'),
+    path('cart/', views.CartListView.as_view(), name='cart'),
+    path('checkout/', views.checkout_cart, name='checkout'),
     path('', views.BooksListView.as_view(), name='index'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,13 +1,14 @@
-from django.views.generic import DetailView, FormView, ListView, CreateView
+from django.views.generic import DetailView, FormView, ListView, CreateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
 from books.models import Book, UserOrder, BookReview
 from django.shortcuts import render, redirect, get_object_or_404
-# Create your views here.
+
 
 def split_into_rows(A, n=4):
     return [A[i:i + n] for i in range(0, len(A), n)]
@@ -23,19 +24,47 @@ class MyBooksListView(LoginRequiredMixin, ListView):
     model = Book
     template_name = 'my_books.html'
 
+    def get_queryset(self):
+        cart_items = UserOrder.objects.filter(user=self.request.user, payed_order=True)
+        books = split_into_rows([item.book for item in cart_items])
+        return books
+
 
 class CartListView(LoginRequiredMixin, ListView):
     model = UserOrder
     template_name = 'cart.html'
+
+    def get_queryset(self):
+        cart_items = UserOrder.objects.filter(user=self.request.user, payed_order=False)
+        books = [item.book for item in cart_items]
+        return books
 
 
 class BookDetailView(LoginRequiredMixin, DetailView):
     model = Book
     template_name = 'book_details.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['now'] = timezone.now()
-        # todo get book reviews here too.
-        print(context)
-        return context
+
+@login_required
+def add_book_to_cart(request, pk):
+    if request.method == 'POST':
+        user_order = UserOrder(user=request.user, payed_order=False, book=Book.objects.get(pk=pk))
+        user_order.save()
+    return redirect('cart')
+
+
+
+@login_required
+def checkout_cart(request):
+    if request.method == 'POST':
+        user_order = UserOrder.objects.filter(user=request.user, payed_order=False)
+        user_order.update(payed_order=True)
+    return redirect('my')
+
+
+class CreateComment(LoginRequiredMixin, CreateView):
+    model = BookReview
+
+class DeleteComment(LoginRequiredMixin, DeleteView):
+    model = BookReview
+
